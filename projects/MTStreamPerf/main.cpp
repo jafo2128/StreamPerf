975a045db7a23f014d6b448b15a7ef8fb6886810
martin.ming <shareviews@sina.com>
2017-12-09 23:52:57 +0800

    Update main.cpp

--- projects/MTStreamPerf/main.cpp
+++ projects/MTStreamPerf/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <stdio.h>
 #include "Define.h"
+#include "MTPerfTaskUDP.h"
 using namespace std;
 
-#define TAG "StreamServer-Win"
+#define TAG "MTStreamPerf"
 
 #define PORT_INSIGHT   8989
 #define PORT_MEDIA_UDP 8080
@@ -19,31 +20,28 @@ class AndroidLogListener : public MTLogListener{
 
 int main(int argc, char *argv[])
 {
-    const char* server_uri = "192.168.0.105";
     AndroidLogListener *listener = new AndroidLogListener();
     MTLog::setLogListener(listener);
 
     MTTaskRunner* scheduler = new MTTaskRunner(5/*max_threads*/, 30/*max_tasks*/);
     scheduler->start();
 
-    MTInsightTaskServer*  perfServer = new MTInsightTaskServer();
-    MTInsightTaskClient*  perfClient = new MTInsightTaskClient();
-    MTMediaUDPTaskServer* udpServer  = new MTMediaUDPTaskServer();
-    MTMediaUDPTaskClient* udpClient  = new MTMediaUDPTaskClient();
-    perfServer->initTaskParams(PORT_INSIGHT, "");
-    perfClient->initTaskParams(PORT_INSIGHT, "");
-    udpServer->initTaskParams(PORT_MEDIA_UDP, server_uri);
-    udpClient->initTaskParams(PORT_MEDIA_UDP, server_uri);
-    scheduler->addTask(PRIORITY_FIFO, perfServer);
-    scheduler->addTask(PRIORITY_FIFO, perfClient);
-    scheduler->addTask(PRIORITY_FIFO, udpServer);
-    scheduler->addTask(PRIORITY_FIFO, udpClient);
+    //MTPerfTask*  taskHTTP = new MTPerfTaskHTTP();
+    //MTPerfTask*  taskRTSP = new MTPerfTaskRTSP();
+    //MTPerfTask*  taskTCP = new MTPerfTaskTCP();
+    MTPerfTask*  server_udp = new MTPerfTaskUDPServer();
+    MTPerfTask*  client_udp = new MTPerfTaskUDPClient();
+    //scheduler->addTask(PRIORITY_FIFO, taskHTTP);
+    //scheduler->addTask(PRIORITY_FIFO, taskRTSP);
+    //scheduler->addTask(PRIORITY_FIFO, taskTCP);
+    scheduler->addTask(PRIORITY_FIFO, server_udp);
+    scheduler->addTask(PRIORITY_FIFO, client_udp);
 
     usleep(2000*1000);
     delete scheduler;
     scheduler = NULL;
-    MTPerfUtil::getInstance()->dumpServerPerf();
-    MTPerfUtil::getInstance()->dumpClientPerf();
+    //MTPerfUtil::getInstance()->dumpServerPerf();
+    //MTPerfUtil::getInstance()->dumpClientPerf();
     //delete MTPerfUtil::getInstance();
     return 0;
 }
